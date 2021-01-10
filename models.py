730d6c724242ee0a18339abd1ace5fcb5218c13c
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-01-09 19:20:34 -0800

    added pylint for SQLAlchemy for better code-completion

--- models.py
+++ models.py
@@ -1,8 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class User(db.Model):
+    """Defines a user instance"""
 
+    __tablename__ = "users"
+
+    id = db.Column()
+
+
+    
