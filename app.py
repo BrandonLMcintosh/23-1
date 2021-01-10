fdd25ca21430360d0bbe2babf592b99c4551263d
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-01-09 22:57:55 -0800

    added routes. Added db functionality

--- app.py
+++ app.py
@@ -1,12 +1,67 @@
 """Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = "SuperSecret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+toolbar = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def index():
+    return redirect("/users")
+
+
+@app.route('/users')
+def users_get():
+    return render_template("user_list.html.j2")
+
+
+@app.route('/users/new')
+def new_user():
+    return render_template("new_user.html.j2")
+
+
+@app.route('/users/new', methods=["POST"])
+def users_post():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url"]
+
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+
+@app.route('/users/<int:user_id>')
+def user_get(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("user.html.j2", user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def user_update(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form["first_name"]
+    user.last_name = request.form["last_name"]
+    user.image_url = request.form["image_url"]
+    db.session.commit()
+    return redirect(f'users/{user_id}')
+
+
+@app.route('/users/<int:user_id>/delete')
+def user_delete(user_id):
+    user = User.query.get_or_404(user_id)
+    
--- models.py
+++ models.py
@@ -1,17 +1,29 @@
+"""Primary DB Class for User object"""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import URLType
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
+    """connects to the db"""
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     """Defines a user instance"""
 
     __tablename__ = "users"
 
-    id = db.Column()
+    def __repr__(self):
+        u = self
+        return f"User {u.first_name} {u.last_name} {u.image_url}"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    
+    first_name = db.Column(db.String(20), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
+
+    image_url = db.Column(URLType)
