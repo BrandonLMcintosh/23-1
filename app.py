2c98f6faef6643f3581f327c73191633eb32ab31
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-01-10 21:30:31 -0800

    resolving routes. /user/<int:user_id>/edit POST route needs work

--- app.py
+++ app.py
@@ -16,21 +16,29 @@ db.create_all()
 
 @app.route('/')
 def index():
+    """REDIRECTS IMMEDIATELY TO /USERS"""
     return redirect("/users")
 
 
 @app.route('/users')
 def users_get():
-    return render_template("user_list.html.j2")
+    """GET ROUTE FOR USER LIST (INDEX)"""
+
+    users = User.query.all()
+    return render_template("user_list.html.j2", users=users)
 
 
 @app.route('/users/new')
-def new_user():
+def users_new():
+    """GET ROUTE FOR NEW USER FORM"""
+    
     return render_template("new_user.html.j2")
 
 
 @app.route('/users/new', methods=["POST"])
 def users_post():
+    """POST ROUTE FOR NEW USER"""
+
     first_name = request.form["first_name"]
     last_name = request.form["last_name"]
     image_url = request.form["image_url"]
@@ -39,6 +47,7 @@ def users_post():
         first_name=first_name,
         last_name=last_name,
         image_url=image_url)
+
     db.session.add(user)
     db.session.commit()
 
@@ -46,22 +55,39 @@ def users_post():
 
 
 @app.route('/users/<int:user_id>')
-def user_get(user_id):
+def users_get_user(user_id):
+    """GET ROUTE FOR INDIVIDUAL USER"""
+
     user = User.query.get_or_404(user_id)
     return render_template("user.html.j2", user=user)
 
 
 @app.route('/users/<int:user_id>/edit')
-def user_update(user_id):
+def users_edit_get(user_id):
+    """GET ROUTE FOR USER EDIT"""
+
     user = User.query.get_or_404(user_id)
+    return render_template("edit_user.html.j2", user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_edit_post(user_id):
+    """POST ROUTE FOR USER EDIT"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.add(user)
     user.first_name = request.form["first_name"]
     user.last_name = request.form["last_name"]
     user.image_url = request.form["image_url"]
     db.session.commit()
-    return redirect(f'users/{user_id}')
+
 
 
 @app.route('/users/<int:user_id>/delete')
-def user_delete(user_id):
+def users_delete_user(user_id):
+    """GET ROUTE FOR USER DELETE"""
+
     user = User.query.get_or_404(user_id)
-    
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/')
